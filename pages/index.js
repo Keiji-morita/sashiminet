226579047ba9e718761be3f2390c5aa31789101c
Keiji-morita <keijim3116@gmail.com>
2022-05-19 15:47:13 +0900

    delete

--- pages/index.js
+++ pages/index.js
@@ -6,58 +6,48 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-<div className="relative bg-white" style={{width: '1440px', height: '5297px'}}>
-    <div className="inline-flex items-end justify-center px-96 pt-36 pb-5 absolute left-0 top-0 bg-gray-100" style={{width: '1440px', height: '198px'}}>
-        <p className="text-2xl">ビジネスサークル</p>
+<div className="inline-flex flex-col space-y-5 items-center justify-start py-16 bg-white" style="width: 1440px; height: 5144px;">
+    <p className="text-4xl text-center">sashiMinet</p>
+    <p className="text-4xl text-center">About</p>
+    <p className="text-4xl text-center">Works</p>
+    <p className="text-4xl text-center">Blog</p>
+    <p className="text-4xl text-center">Contact</p>
+    <div className="bg-gray-200" style="width: 759px; height: 590px;"/>
+    <p className="text-6xl font-black text-center">SashiMinet</p>
+    <p className="text-xl text-center">web design and bussiness</p>
+    <p className="text-xl" style="width: 463px;">テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    <p className="text-4xl">About</p>
+    <p className="text-4xl">About</p>
+    <div className="rounded-2xl border-black" style="width: 444px; height: 1px;">
+        <div className="border-black" style="width: 444px; height: 1px;"/>
     </div>
-    <div className="relative bg-gray-100" style={{width: '1440px', height: '892px', left: '0px', top: '198px'}}>
-        <div className="absolute left-0 top-0 bg-white" style={{width: '1440px', height:'434px'}}/>
-        <p className="absolute text-4xl text-center" style={{width: '615px', height: '51px', left: '412px', top: '270px'}}>企業から人へ、人から未来へ。</p>
-        <p className="absolute text-2xl text-center" style={{width: '615px', height: '51px', left: '412px', top: '333px'}}>テキストテキストテキストテキスト<br/>テキストテキストテキストテキスト</p>
-        <div className="w-48 h-40 absolute bg-gray-300" style={{left: '623px', top: '70px'}}/>
+    <div className="flex flex-col space-y-8 items-end justify-end pl-96 pr-28 pt-32 pb-16 bg-gray-100" style="width: 1440px; height: 437px;">
+        <p className="text-4xl text-center" style="width: 615px; height: 51px;">150万円も払える学生はいない</p>
+        <p className="text-2xl text-center" style="width: 615px; height: 147px;">一般的なweb制作会社では最低価格は150万円もします。これで夢を諦めてしまう学生は大勢います。そんな学生を私たちは制作費3万円でご支援します。</p>
     </div>
-    <div className="inline-flex flex-col space-y-10 items-end justify-start pt-48 pb-36 absolute bg-gray-100" style={{width: '1440px', height: '936px', left: '0px', top: '1916px'}}>
-        <p className="text-4xl text-center" style={{width: "615px", height: "51px"}}>企業から人へ、人から未来へ。</p>
-        <p className="text-2xl text-center" style={{width: "615px", height: "51px"}}>テキストテキストテキストテキスト<br/>テキストテキストテキストテキスト</p>
-        <div className="w-24 h-1/6 bg-gray-300"/>
-        <div className="bg-white" style={{width: "1440px", height: "91px"}}/>
-        <p className="text-4xl text-center" style={{width: "615px", height: "51px"}}>企業から人へ、人から未来へ。</p>
-        <p className="text-2xl text-center" style={{width: "615px", height: "51px"}}>テキストテキストテキストテキスト<br/>テキストテキストテキストテキスト</p>
+    <p className="text-4xl">why</p>
+    <div className="border-black" style="width: 444px; height: 1px;"/>
+    <div className="flex flex-col space-y-9 items-end justify-end pl-96 pr-28 pt-32 pb-20 bg-gray-100" style="width: 1440px; height: 437px;">
+        <p className="text-4xl text-center" style="width: 615px; height: 51px;">どうして3万円で作成できるのか</p>
+        <p className="text-2xl text-center" style="width: 615px; height: 140px;">多くのweb制作会社では中ぬきが行われています。私たちは中ぬきを一切行わずダイレクトにお客様とエンジニアを繋げます。だから、我々は格安で作成することができるのです。クオリティが落ちることは一切ございません。</p>
     </div>
-    <div className="inline-flex flex-col space-y-14 items-center justify-end pt-16 pb-11 absolute bg-gray-100" style="width: 1440px; height: 829px; left: 0px; top: 1088px;">
-        <p className="text-4xl">About</p>
-        <div className="border-black" style={{width: "444px", height: "1px"}}/>
-        <p className="text-4xl text-center" style={{width: "615px", height: "51px"}}>企業から人へ、人から未来へ。</p>
-        <p className="text-2xl text-center" style={{width: "615px", height: "51px"}}>テキストテキストテキストテキスト<br/>テキストテキストテキストテキスト</p>
-        <div className="bg-white" style={{width: "1440px", height: "198px"}}/>
-        <p className="text-4xl">活動内容</p>
-        <div className="border-black" style={{width: "444px", height: "1px"}}/>
-    </div>
-    <p className="absolute text-4xl" style={{left: "560px", top: "2961px"}}>代表プロフィール</p>
-    <div className="inline-flex flex-col space-y-4 items-center justify-end px-96 pt-96 pb-60 absolute bg-gray-100" style={{width: "1440px", height: "829px", left: "0px", top: "3920px"}}>
-        <p className="text-4xl text-center" style={{width: "615px", height: "51px"}}>企業から人へ、人から未来へ。</p>
-        <p className="text-2xl text-center" style={{width: "615px", height: "51px"}}>テキストテキストテキストテキスト<br/>テキストテキストテキストテキスト</p>
-        <div className="w-1/2 h-1/4 bg-gray-800 rounded-3xl"/>
+    <p className="text-2xl text-center" style="width: 615px; height: 117px;">安心の後払い制度、(不安ですよね。お客様が納得してからお支払いいただきます。)<br/></p>
+    <p className="text-4xl">私たちについて</p>
+    <div className="border-black" style="width: 444px; height: 1px;"/>
+    <div className="w-1/5 h-80 bg-gray-300 rounded-tl-3xl rounded-tr-3xl rounded-bl-3xl rounded-br-full"/>
+    <div className="w-1/5 h-80 bg-gray-300 rounded-tl-3xl rounded-tr-3xl rounded-bl-3xl rounded-br-full"/>
+    <p className="w-1/5 h-14 text-5xl font-w4 text-center">中田敦彦</p>
+    <p className="w-1/5 h-14 text-5xl font-w4 text-center">中田敦彦</p>
+    <p className="w-80 text-4xl font-w4 text-center text-gray-700">テキストテキストテキストテキストテキストテキスト</p>
+    <p className="w-80 text-4xl font-w4 text-center text-gray-700">テキストテキストテキストテキストテキストテキスト</p>
+    <div className="flex flex-col space-y-5 items-center justify-end px-80 pt-80 pb-60 bg-gray-100" style="width: 1440px; height: 829px;">
+        <p className="text-4xl text-center" style="width: 757px; height: 51px;">私たちからあなたへ、あなたから人へ。</p>
+        <p className="text-2xl text-center" style="width: 615px; height: 51px;">まずは、お話を聞かせてください。<br/>あなたにご支援をさせてください。</p>
+        <div className="w-80 h-14 bg-gray-800 rounded-3xl"/>
         <p className="text-xl font-black text-white">コンタクト</p>
     </div>
-    <p className="w-72 h-14 absolute text-5xl font-w4 text-center" style={{left: "146px", top: "3523px"}}>中田敦彦</p>
-    <p className="w-72 h-14 absolute text-4xl font-w4 text-center text-gray-700" style={{left: "146px", top: "3464px"}}>副代表</p>
-    <div className="absolute bg-white" style={{width: "1440px", height: "198px", left: "0px", top: "1087px"}}/>
-    <div className="absolute border-black" style={{width: "444px", height: "1px", left: "498px", top: "3034px"}}/>
-    <div className="w-72 h-80 absolute bg-gray-300 rounded-tl-3xl rounded-tr-3xl rounded-bl-3xl rounded-br-full" style={{left: "146px", top: "3112px"}}/>
-    <p className="w-72 h-14 absolute text-5xl font-w4 text-center" style={{left: "575px", top: "3523px"}}>中田敦彦</p>
-    <p className="w-72 h-14 absolute text-4xl font-w4 text-center text-gray-700" style={{left: "575px", top: "3464px"}}>代表</p>
-    <div className="w-72 h-80 absolute bg-gray-300 rounded-tl-3xl rounded-tr-3xl rounded-bl-3xl rounded-br-full" style={{left: "575px", top: "3112px"}}/>
-    <p className="w-72 h-14 absolute text-5xl font-w4 text-center" style={{left: "1004px", top: "3523px"}}>中田敦彦</p>
-    <p className="w-72 h-14 absolute text-4xl font-w4 text-center text-gray-700" style={{left: "1004px", top: "3464px"}}>サブ代表</p>
-    <div className="w-72 h-80 absolute bg-gray-300 rounded-tl-3xl rounded-tr-3xl rounded-bl-3xl rounded-br-full" style={{left: "1004px", top: "3112px"}}/>
-    <p className="w-80 absolute text-4xl font-w4 text-center text-gray-700" style={{left: "124px", top: "3600px"}}>テキストテキストテキストテキストテキストテキスト</p>
-    <p className="w-80 absolute text-4xl font-w4 text-center text-gray-700" style={{left: "553px", top: "3598px"}}>テキストテキストテキストテキストテキストテキスト</p>
-    <p className="w-80 absolute text-4xl font-w4 text-center text-gray-700" style={{left: "982px", top: "3600px"}}>テキストテキストテキストテキストテキストテキスト</p>
-    <img className="absolute" style={{width: "515px", height: "257px", left: "462px", top: "4848px"}} src="https://via.placeholder.com/515x257"/>
-    <p className="absolute text-4xl font-black" style={{left: "619px", top: "5149px"}}>sashiMinet</p>
-    <p className="absolute text-xl font-black" style={{left: "525px", top: "5205px"}}>@2021 sashiMinet. All rights reserved.</p>
+    <p className="text-4xl font-black">sashiMinet</p>
+    <p className="text-xl font-black">@2021 sashiMinet. All rights reserved.</p>
 </div>
-
   )
 }
